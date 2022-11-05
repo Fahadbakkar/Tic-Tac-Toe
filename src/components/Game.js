38f923e11d70a1825b519e7373503556c9e5e1b1
Fahad <fahadbakkar@hotmail.com>
2022-11-05 16:06:15 -0400

    first commit

---
+++ src/components/Game.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useState } from "react";
+import Board from "./Board";
+import calculateWinner from "../helper";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[i]) return;
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : `Go to Start:`;
+      return (
+        <li key={move}>
+          <button className="Button" onClick={() => jumpTo(move)}>
+            {destination}
+          </button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <div className="header">
+        <h1>React Tic Tac Toe</h1>
+        <button className="reset" onClick={() => window.location.reload()}>
+          Reset
+        </button>
+      </div>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>
+            History
+            {renderMoves()}
+          </h3>
+        </div>
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO} </h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
